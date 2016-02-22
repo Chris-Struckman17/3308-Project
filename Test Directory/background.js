24b30c435e8f3383106086a929fd7037184b985b
Chris Struckman <chrisstruckman@rl1-131-48-dhcp.int.colorado.edu>
2016-02-22 11:05:30 -0700

    Made changes to the html and css files in order for a better looking website!

--- Test Directory/background.js
+++
@@ -1,8 +0,0 @@
-//background.js
-
-chrome.browserAction.onClicked.addListener(function(tab)){
-	chrome.tabs.query({active: true, currentWindow: true},){
-		var activeTab = tabs[0];
-		chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});	
-	});
-});
--- Test Directory/content.js
+++
@@ -1,1 +0,0 @@
-alert("Welcome to the new and improved Soundcloud!")
