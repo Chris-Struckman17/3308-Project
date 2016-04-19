ad15a0936c6d41cc6a4a40e32dec893bcc0980f2
John <user@rl1-135-61-dhcp.int.colorado.edu>
2016-04-19 17:57:49 -0600

    made it look a bit nicer

--- website/scripts.js
+++ website/scripts.js
@@ -46,8 +46,7 @@ function search(){
   	    var songid = track.id;
   	    songarray[i++] = songid;
   	    console.log(songarray[0]);
-  	    $('#results').append($('<button class="btn btn-clean" type="button" onclick = "songbutton('+track.id+')">Play </button>').html(track.title));
-  	    $('#playerctrl').html($('<iframe width="100%" height="250" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/' + songarray[0] + '&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>'));
+  	    $('#results').append($('<button class="btn btn-clean" type="button" onclick = "songbutton('+track.id+')"></button>').html(track.title));
   	    $('#results').append('<br>');
   	  });
     });
